82b73fdd61d5404034784f8c3b8045c0f327ba40
Siphesihle Mbuyisa <siphesihlembuyisa@Sphes-MacBook-Air.local>
2023-05-05 00:54:09 +0200

    new update

--- pages/index.js
+++ pages/index.js
@@ -1,118 +1,111 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import Head from 'next/head'
+import { AiFillFire } from 'react-icons/ai'
+import { BsArrowRightShort } from "react-icons/bs"
+import { useSpring, animated, config } from 'react-spring';
 
 export default function Home() {
+  function getYOffset(element) {
+    const rect = element.getBoundingClientRect();
+    const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    return rect.top + scrollTop;
+  }
+
+  //Nav Animation
+  const logoAnimation = useSpring({
+    from: { opacity: 0, transform: 'translateY(130px)' },
+    to: { opacity: 1, transform: 'translateY(0)' },
+    delay: 0
+  })
+  const navAnimation1 = useSpring({
+    from: { opacity: 0, transform: 'translateY(130px)' },
+    to: { opacity: 1, transform: 'translateY(0)' },
+    delay: 50
+  })
+  const navAnimation2 = useSpring({
+    from: { opacity: 0, transform: 'translateY(130px)' },
+    to: { opacity: 1, transform: 'translateY(0)' },
+    delay: 100
+  })
+  const navAnimation3 = useSpring({
+    from: { opacity: 0, transform: 'translateY(130px)' },
+    to: { opacity: 1, transform: 'translateY(0)' },
+    delay: 150
+  })
+
+  //Section Animation
+  const homeAnimation = useSpring({
+    from: { opacity: 0, transform: 'translateY(130px)' },
+    to: { opacity: 1, transform: 'translateY(0)' },
+    delay: 600
+  })
+  const projectsAnimation = useSpring({
+    from: { opacity: 0, transform: 'translateY(130px)' },
+    to: { opacity: 1, transform: 'translateY(0)' },
+    delay: 1000
+  })
+  const contactAnimation = useSpring({
+    from: { opacity: 0, transform: 'translateY(130px)' },
+    to: { opacity: 1, transform: 'translateY(0)' },
+    delay: 1200
+  })
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      <Head>
+        <title>S T O I C</title>
+        <meta name="description" content="Sphe Editig Home Page" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      {/* Navbar */}
+      <div className="flex justify-between items-center p-8 text-sm">
+        <animated.a style={logoAnimation} href={'/'}>S T O I C</animated.a>
+        <div className='flex gap-5 mask'>
+          <animated.button style={navAnimation1} className="font-medium uppercase hover:underline" onClick={() => window.scrollTo({ top: getYOffset(document.getElementById('home')), behavior: 'smooth' })}>Home</animated.button>
+          <animated.button style={navAnimation2} className="font-medium uppercase hover:underline text-[#727272]" onClick={() => window.scrollTo({ top: getYOffset(document.getElementById('projects')), behavior: 'smooth' })}>Projects</animated.button>
+          <animated.button style={navAnimation3} className="font-medium uppercase hover:underline text-[#727272]" onClick={() => window.scrollTo({ top: getYOffset(document.getElementById('contact')), behavior: 'smooth' })}>Contact</animated.button>
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      {/* Home */}
+      <animated.div style={homeAnimation} id="home" className="bg-[#131315] mb-5 mx-7 rounded-3xl flex flex-col justify-center items-center md:p-5">
+        <div className="p-16 text-center flex flex-col justify-center items-center">
+        <h1 className="text-white text-4xl md:text-6xl font-bold">Brand growth through <br/> Web Development.</h1>
+          <p className="text-[#54545D] m-3">Crafting beautiful and responsive websites, tailored to your
+            business needs.<br /> Specializing in modern web technologies to help
+            bring your ideas to life.</p>
+          <button className="text-white bg-[var(--highlight)] w-fit py-3 m-4 rounded-full flex justify-center items-center gap-3 px-5 transition-all hover:scale-110" onClick={() => window.location.href = 'mailto:sphe.g.personal@gmail.com'}>
+            Contact Me
+            <BsArrowRightShort className="text-[var(--highlight)] bg-white rounded-full" size={25}/>
+          </button>
+        </div>
+      </animated.div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
+      {/* Projects */}
+      <animated.div style={projectsAnimation} id="projects" className="flex flex-col md:flex-row justify-between w-full gap-5 px-7 pb-5">
+        <a className='sphe-editing w-full p-5 rounded-3xl h-[300px] flex justify-start items-end shadow-sm' href="https://sphe.vercel.app" target="_blank" rel="noopener noreferrer">
+            <div className="flex justify-between items-center w-full">
+              <h1 className="text-white text-2xl font-medium cursor-default">Sphe Editing Website</h1>
+              <BsArrowRightShort className="text-[var(--highlight)] bg-white rounded-full transition-all hover:scale-125" size={30} />
+            </div>
         </a>
+        <a className="sphe-portfolio w-full p-5 rounded-3xl h-[300px] flex justify-start items-end shadow-sm" href="https://sphe-portfolio.vercel.app" target="_blank" rel="noopener noreferrer">
+            <div className="flex justify-between items-center w-full">
+              <h1 className="text-white text-2xl font-medium cursor-default">Sphe Portfolio Website</h1>
+              <BsArrowRightShort className="text-[var(--highlight)] bg-white rounded-full transition-all hover:scale-125" size={30} />
+            </div>
+        </a>
+      </animated.div>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+        {/* Contact */}
+        <animated.div style={contactAnimation} id="contact" className="bg-[#131315] mb-5 mx-7 rounded-3xl flex flex-col justify-center items-center p-20">
+          <h1 className="text-white text-4xl md:text-6xl font-bold">Have a cool project?</h1>
+          <p className="text-[#54545D] m-3">Feel free to reach out for collaborations or just a friendly hello</p>
+          <button className="text-white bg-[var(--highlight)] w-fit py-3 mt-4 rounded-full flex justify-center items-center gap-3 px-5 transition-all hover:scale-110" onClick={() => window.location.href = 'mailto:sphe.g.personal@gmail.com'}>
+            Contact Me
+            <BsArrowRightShort className="text-[var(--highlight)] bg-white rounded-full" size={25}/>
+          </button>
+        </animated.div>
+    </>  )
 }
